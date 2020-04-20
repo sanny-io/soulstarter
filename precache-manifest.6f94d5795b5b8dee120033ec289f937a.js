5572c347734eb29b03a404791d9db861694fb5e8
sanny-io <sannysherief@gmail.com>
2020-04-20 14:41:06 -0700

    Updates

---
+++ precache-manifest.6f94d5795b5b8dee120033ec289f937a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac3f8280914f8ea9e88963c5bf70649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761b644bb08288bff5db",
+    "url": "/static/css/main.abe67bf5.chunk.css"
+  },
+  {
+    "revision": "f812e1d4ebe32fcbe31f",
+    "url": "/static/js/2.97023cfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97023cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761b644bb08288bff5db",
+    "url": "/static/js/main.b423b4b8.chunk.js"
+  },
+  {
+    "revision": "8d818148373d7cce0f78",
+    "url": "/static/js/runtime-main.341f2df4.js"
+  }
+]);
